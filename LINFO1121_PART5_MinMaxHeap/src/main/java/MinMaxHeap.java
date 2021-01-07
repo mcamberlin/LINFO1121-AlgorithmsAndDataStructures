c0c00b9d32426131387684b8927fd67b632234cf
mcamberlin <merlin.camberlin@student.uclouvain.be>
2021-01-07 20:50:10 +0100

    Update MinMaxHeap.java

--- LINFO1121_PART5_MinMaxHeap/src/main/java/MinMaxHeap.java
+++ LINFO1121_PART5_MinMaxHeap/src/main/java/MinMaxHeap.java
@@ -18,21 +18,27 @@ public class MinMaxHeap<Key extends Comparable<Key>> {
     /**
      * @pre size() >= 1
      */
-    public Key min() {
+    public Key min()
+    {
         //TODO O(1) expected
+        if (isEmpty())
+            return null;
         return pq[1];
     }
 
     /**
      * @pre size() >= 1
      */
-    public Key max() {
+    public Key max()
+    {
         //TODO O(1) expected
-        if (size()==1)
+        if (isEmpty())
+            return null;
+        else if (size() == 1)
             return pq[1];
-        else if (size()==2)
+        else if (size() == 2)
             return pq[2];
-        else if( less(2,3))
+        else if (pq[2].compareTo(pq[3]) < 0)
             return pq[3];
         else
             return pq[2];
@@ -41,83 +47,81 @@ public class MinMaxHeap<Key extends Comparable<Key>> {
     /**
      * @pre 1 <= k <= size()
      */
-    private void swim(int k) {
+    private void swim(int k)
+    {
         //TODO O(log N) expected
-        if(isMinLevel(k))
-            swimMin(k);
-        else
-            swimMax(k);
+        //1. Determine in which level we are
+        int level = level(k);
+        if (level % 2 == 0) // => insertion in a min level
+        {
+            int i;
+            //check min levels
+            for (i = k; i / 4 > 0 && pq[i].compareTo(pq[i / 4]) < 0; i = i / 4)
+            {
+                exch(i, i / 4);
+            } // correctly positionned
+
+            //check max levels
+            if (i / 2 > 0 && pq[i].compareTo(pq[i / 2]) > 0)
+            {
+                exch(i, i / 2);
+                swim(i / 2);
+            }
+        } else // insertion in a max level
+        {
+            int i;
+            //check max levels
+            for (i = k; i / 4 > 0 && pq[i].compareTo(pq[i / 4]) > 0; i = i / 4)
+            {
+                exch(i, i / 4);
+            } // correctly positionned
+
+            //check min levels
+            if (i / 2 > 0 && pq[i].compareTo(pq[i / 2]) < 0)
+            {
+                exch(i, i / 2);
+                swim(i / 2);
+            }
+
+        }
+
     }
 
-    private void swimMin(int k)
+    public static int level(int k)
     {
-        if(k>1 && less(k/2, k))
-        {
-            exch(k, k / 2);
-            swimMax(k/2);
-        }
-        else if (k>3 && less(k, k/4))
-        {
-            exch(k, k/4);
-            swimMin(k/4);
-        }
-    }
-
-    private void swimMax(int k)
-    {
-        if(k>1 && less(k, k/2))
-        {
-            exch(k, k / 2);
-            swimMin(k/2);
-        }
-        else if (k>7 && less(k/4, k))
-        {
-            exch(k, k/4);
-            swimMax(k/4);
-        }
-    }
-
-    private boolean isMinLevel(int k)
-    {
+        if (k == 1)
+            return 1;
         int level = 0;
-
-        while(k>= power(level))
+        while (k != 1)
         {
+            k = k / 2;
             level++;
         }
-        if(level%2==0)
-            return true;
-        else
-            return false;
-    }
-
-    private int power(int b)
-    {
-        int c;
-        for(c=2; b>0; b--)
-            c=c*2;
-        return c;
+        return level;
     }
 
     // ---------------------------------------------------------------------------------------
     // Constructor and helpers. You should not modify this. However, using them may be useful.
     // ---------------------------------------------------------------------------------------
 
-    public MinMaxHeap(int maxN) {
-        pq = (Key[]) new Comparable[maxN + 1];
+    public MinMaxHeap(int maxN)
+    {
+        this.pq = (Key[]) new Comparable[maxN + 1];
     }
 
     /**
      * @return pq[i] < pq[j]
      */
-    public boolean less(int i, int j) {
+    public boolean less(int i, int j)
+    {
         return pq[i].compareTo(pq[j]) < 0;
     }
 
     /**
      * Exchanges positions i and j in pq
      */
-    private void exch(int i, int j) {
+    private void exch(int i, int j)
+    {
         Key e = pq[i];
         pq[i] = pq[j];
         pq[j] = e;
@@ -126,27 +130,32 @@ public class MinMaxHeap<Key extends Comparable<Key>> {
     /**
      * @return true if the heap is empty, false else
      */
-    public boolean isEmpty() {
+    public boolean isEmpty()
+    {
         return N == 0;
     }
+
     /**
      * @return the size of the heap
      */
-    public int size() {
+    public int size()
+    {
         return N;
     }
 
     /**
      * @param v value to insert in the heap. v != null.
      */
-    public void insert(Key v) {
+    public void insert(Key v)
+    {
         pq[++N] = v;
         if (N >= (1 << height)) height++;
         swim(N);
     }
 
     @Override
-    public String toString() {
+    public String toString()
+    {
         return Arrays.toString(pq);
     }
 }
